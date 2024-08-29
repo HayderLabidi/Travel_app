26fc8ebd7848a818aa6b860b92d5ec67c931d5ad
Hayder Labidi <108490838+HayderLabidi@users.noreply.github.com>
2024-08-29 10:59:22 +0100

    y

--- lib/home.dart
+++ lib/home.dart
@@ -3,7 +3,6 @@ import 'package:travel/Homepage/mainpage.dart';
 import 'package:travel/Homepage/messages.dart';
 import 'package:travel/Homepage/profile.dart';
 
-
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -11,10 +10,15 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
-int _selectedindex = 0;
-List<Widget> _screens = [const Mainpage(), const MessengerPage(), const ProfileView()];
-
 class _HomeState extends State<Home> {
+  int _selectedindex = 0; // Default index for Mainpage
+
+  List<Widget> _screens = [
+    const Mainpage(),
+    const MessengerPage(),
+    const ProfileView(),
+  ];
+
   void _onItemTaped(int index) {
     setState(() {
       _selectedindex = index;
@@ -24,7 +28,6 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      
       child: Scaffold(
         bottomNavigationBar: BottomNavigationBar(
           backgroundColor: Colors.black,
@@ -33,14 +36,19 @@ class _HomeState extends State<Home> {
           items: const [
             BottomNavigationBarItem(
               icon: Icon(Icons.home),
-              label: "home",
+              label: "Home",
             ),
-            BottomNavigationBarItem(icon: Icon(Icons.mail), label: "Messages"),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.mail),
+              label: "Messages",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "Profile",
+            ),
           ],
           currentIndex: _selectedindex,
           onTap: _onItemTaped,
-          
         ),
         body: _screens[_selectedindex],
       ),
